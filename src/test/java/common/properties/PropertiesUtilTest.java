3b2fddd5e5e4fc5d2b9b22d8eea8904af87e8571
zyx <zhaoxu_sun@163.com>
2017-12-29 22:57:03 +0800

    add Properties

---
+++ src/test/java/common/properties/PropertiesUtilTest.java
@@ -0,0 +1,32 @@
+package common.properties;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PropertiesUtilTest {
+    @Test
+    public void testGetProperty() throws Exception {
+    }
+
+    @Test
+    public void testSetProperty() throws Exception {
+    }
+
+    @Test
+    public void testRemoveProperty() throws Exception {
+    }
+
+    @Test
+    public void testGetProperties() throws Exception {
+    }
+
+    @Test
+    public void testSetProperties() throws Exception {
+    }
+
+    @Test
+    public void testGetPropertiesKeySet() throws Exception {
+    }
+
+}
